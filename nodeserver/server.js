d1eb2109a2a157fb39c5374ca8e404e7fa285502
Kawsar <kawsar@macbooks-air.home>
2020-10-29 17:12:15 +0000

    hello

---
+++ nodeserver/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const HOSTNAME = '127.0.0.1';
+const PORT = '8080';
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end ('Hello World!');
+
+});
+
+server.listen(port, hostname , () => {
+    console.log('Server running at http://${hostname}: ${port}`);
+
+});
